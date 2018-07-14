381509bfa2c5372eff73a0e4c7e10aa54e771572
dns <dns@dnss-MacBook-Pro.local>
2018-07-14 15:55:37 -0400

    updated code - bug fixes

--- app/.externalNativeBuild/cmake/x86Release/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/x86Release/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,13 +1,13 @@
-set(CMAKE_HOST_SYSTEM "Darwin-16.0.0")
+set(CMAKE_HOST_SYSTEM "Darwin-17.7.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "16.0.0")
+set(CMAKE_HOST_SYSTEM_VERSION "17.7.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/Users/dns/Library/Android/sdk/cmake/3.6.3155560/android.toolchain.cmake")
+include("/Users/dns/Library/Android/sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
-set(CMAKE_SYSTEM "Android-15")
+set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
-set(CMAKE_SYSTEM_VERSION "15")
+set(CMAKE_SYSTEM_VERSION "1")
 set(CMAKE_SYSTEM_PROCESSOR "i686")
 
 set(CMAKE_CROSSCOMPILING "TRUE")
